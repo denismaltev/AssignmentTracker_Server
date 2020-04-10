884c55802bb5f60d28e2391c217b9e0c289747ca
Den79 <test08081979@gmail.com>
2020-04-10 16:11:01 -0700

    mongoose schema

---
+++ models/assignment.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const assignmentSchema = new Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  isDone: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
+
+module.exports = mongoose.model("Assignment", assignmentSchema);
