1a55b0d0755582e9b429becb2ca4bcae2a3d6bf3
Den79 <test08081979@gmail.com>
2020-04-10 21:28:08 -0700

    POST assignment

--- models/assignment.js
+++ models/assignment.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const assignmentSchema = new Schema({
-  userId: {
-    type: String,
-    required: true
-  },
+  // userId: {
+  //   type: String,
+  //   required: true
+  // },
   title: {
     type: String,
     required: true
--- routes/assignments.js
+++ routes/assignments.js
@@ -7,11 +7,6 @@ var path = require("path");
 let configPath = path.join(__dirname, "../", ".env");
 dotenv.config({ path: configPath });
 
-var MongoClient = require("mongodb").MongoClient;
-var db_server = process.env.DB_SERVER || "localhost";
-var url = "mongodb://" + db_server + ":27017";
-const ObjectId = require("mongodb").ObjectID;
-
 // Get all assignments
 router.get("/assignments", (req, res) => {
   MongooseAssignmentModel.find({}, (err, data) => {
@@ -20,42 +15,14 @@ router.get("/assignments", (req, res) => {
   });
 });
 
-// // Get all assignments
-// router.get("/assignments", function(req, res, next) {
-//   //res.send("ASSIGNMENTS API");
-//   MongoClient.connect(url, function(connecterr, db) {
-//     if (connecterr) throw connecterr;
-//     var dbo = db.db("assignments"); //Base Name
-//     dbo
-//       .collection("assignments") // Table name
-//       .find({})
-//       .toArray(function(err, data) {
-//         if (err) res.send(err);
-//         res.json(data);
-//         db.close();
-//       });
-//   });
-// });
-
-// // Get ONE assignment by Id
-// router.get("/assignments/:id", function(req, res, next) {
-//   MongoClient.connect(url, function(connecterr, db) {
-//     if (connecterr) throw connecterr;
-//     var dbo = db.db("assignments");
-//     const id = new ObjectId(req.params.id);
-//     var query = { _id: id };
-//     dbo
-//       .collection("assignments")
-//       .find(query)
-//       .toArray(function(err, data) {
-//         if (err) res.send(err);
-//         res.json(data);
-//         db.close();
-//       });
-//   });
-// });
-
 // Create an assignment
-router.post("/assignments");
+router.post("/assignments", (req, res) => {
+  var assignment = req.body;
+  let newAssignment = new MongooseAssignmentModel(assignment);
+  newAssignment.save((err, data) => {
+    if (err) res.send(err);
+    res.json(201, data);
+  });
+});
 
 module.exports = router;
